443c9740ec0a9677ae23530be0575d723e14e433
QihangYang <yqhziyou13@gmail.com>
2024-12-06 22:02:56 -0500

    idk

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,9 +6,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe({
-    whitelist: true, // 自动删除DTO中未声明的属性
-    forbidNonWhitelisted: true, // 阻止传入DTO中未声明的属性
-    transform: true, // 自动转换DTO
+    whitelist: true, 
+    forbidNonWhitelisted: true, 
+    transform: true, 
   }));
   
   await app.listen(process.env.PORT ?? 3000);
--- frontend/src/api/user.ts
+++ frontend/src/api/user.ts
@@ -3,38 +3,38 @@ import axios from 'axios';
 
 const baseURL = 'http://localhost:3000';
 
-// 根据用户 ID 获取用户信息
+
 export const getUserInfo = async (userId: number) => {
     const response = await axios.get(`${baseURL}/users/${userId}`);
     return response.data;
 };
 
-// 创建新用户
+
 export const createUser = async (CreateUserDTO: object) => {
     const response = await axios.post(`${baseURL}/users`, CreateUserDTO);
     return response.data;
 };
 
-//login
+
 export const login = async (CreateUserDTO: object) => {
     const response = await axios.post(`${baseURL}/users/login`, CreateUserDTO);
     return response.data;
 };
 
 
-// 更新用户信息
+
 export const updateUser = async (userId: number, CreateUserDTO: object) => {
     const response = await axios.put(`${baseURL}/users/${userId}`, CreateUserDTO);
     return response.data;
 };
 
-// 删除用户
+
 export const deleteUser = async (userId: number) => {
     const response = await axios.delete(`${baseURL}/users/${userId}`);
     return response.data;
 };
 
-// 根据用户 ID 获取会话列表
+
 export const getUserSessions = async (userId: number) => {
     const response = await axios.get(`${baseURL}/users/${userId}/list`);
     return response.data;
